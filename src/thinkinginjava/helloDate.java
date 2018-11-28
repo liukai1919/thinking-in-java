9fb9422840e48caf0b73fa3401af72e3ebfccfbc
liukai1919 <liukai1919@gmail.com>
2018-11-28 23:30:19 +0800

    first commit

---
+++ src/thinkinginjava/helloDate.java
@@ -0,0 +1,10 @@
+package thinkinginjava;
+
+import java.util.Date;
+
+public class helloDate {
+	public static void main(String[] arg) {
+		System.out.println("Hello it's:");
+		System.out.println(new Date());
+	}
+}
